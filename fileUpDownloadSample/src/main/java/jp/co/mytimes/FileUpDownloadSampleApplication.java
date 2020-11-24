f0cccbf257d758349c28701a3f0013acad5fb154
xpok3 <xpok3@192.168.10.106>
2020-11-25 07:05:44 +0900

    init push

---
+++ fileUpDownloadSample/src/main/java/jp/co/mytimes/FileUpDownloadSampleApplication.java
@@ -0,0 +1,13 @@
+package jp.co.mytimes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileUpDownloadSampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FileUpDownloadSampleApplication.class, args);
+	}
+
+}
---
+++ fileUpDownloadSample/src/test/java/jp/co/mytimes/FileUpDownloadSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package jp.co.mytimes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FileUpDownloadSampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
