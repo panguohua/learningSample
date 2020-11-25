b014975a51f903b55440562a00034dead7c29d7b
xpok3 <xpok3@192.168.10.106>
2020-11-26 08:09:28 +0900

    init

---
+++ imgUploadSample/src/main/java/jp/co/mytimes/ImgUploadSampleApplication.java
@@ -0,0 +1,13 @@
+package jp.co.mytimes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImgUploadSampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImgUploadSampleApplication.class, args);
+	}
+
+}
---
+++ imgUploadSample/src/test/java/jp/co/mytimes/ImgUploadSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package jp.co.mytimes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImgUploadSampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
